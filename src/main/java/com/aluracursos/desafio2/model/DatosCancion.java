45e4f912d13f3b0100895c9b59ce0cd639fa8e0a
juanse951 <juanse951@gmail.com>
2024-11-27 22:37:01 -0500

    se modela la clase cancion

--- src/main/java/com/aluracursos/desafio2/model/DatosCancion.java
+++ src/main/java/com/aluracursos/desafio2/model/DatosCancion.java
@@ -1,4 +1,34 @@
 package com.aluracursos.desafio2.model;
 
 public class DatosCancion {
+
+    private String titulo;
+    private DatosCantante cantante;
+
+    public DatosCancion(String nombreCancion){
+        this.titulo = nombreCancion;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public DatosCantante getCantante() {
+        return cantante;
+    }
+
+    public void setCantante(DatosCantante cantante) {
+        this.cantante = cantante;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "titulo='" + titulo + '\'' +
+                ", cantante=" + cantante.getNombre();
+    }
 }
