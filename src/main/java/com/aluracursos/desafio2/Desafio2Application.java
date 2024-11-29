c55efcca4b4cfad15c0129b4cbe78ee435ab5e12
juanse951 <juanse951@gmail.com>
2024-11-29 08:22:19 -0500

    inyeccion de dependencias,para usar los metodos de esa interface en la clase principal,se debe pasar el atributo repository en el contructor de la clase principal,despues crear ese contructor de repository en la clase principal, ya en esta clase creamos una instancia del repositorio y la enlazamos con this en el constructor

--- src/main/java/com/aluracursos/desafio2/Desafio2Application.java
+++ src/main/java/com/aluracursos/desafio2/Desafio2Application.java
@@ -1,6 +1,8 @@
 package com.aluracursos.desafio2;
 
 import com.aluracursos.desafio2.principal.Principal;
+import com.aluracursos.desafio2.repository.DatosCantanteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,13 +10,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Desafio2Application implements CommandLineRunner {
 
+	@Autowired
+	private DatosCantanteRepository repository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(Desafio2Application.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		Principal principal = new Principal();
+		Principal principal = new Principal(repository);
 		principal.muestraElMenu();
 	}
 }
