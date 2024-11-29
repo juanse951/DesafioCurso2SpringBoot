9c7db5e9e6b550d9cb7b1af6a2b1c7f3352f5e4a
juanse951 <juanse951@gmail.com>
2024-11-28 23:33:19 -0500

    Mapeo de clases a postgress, se crea relacion OneToMany y ManyToOne, se verifica en la base de datos y si se creo correctamente

--- src/main/java/com/aluracursos/desafio2/model/DatosCancion.java
+++ src/main/java/com/aluracursos/desafio2/model/DatosCancion.java
@@ -1,12 +1,29 @@
 package com.aluracursos.desafio2.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "canciones")
 public class DatosCancion {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
     private String titulo;
+    @ManyToOne
     private DatosCantante cantante;
 
+    public DatosCancion(){}
     public DatosCancion(String nombreCancion){
         this.titulo = nombreCancion;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
     }
 
     public String getTitulo() {
--- src/main/java/com/aluracursos/desafio2/model/DatosCantante.java
+++ src/main/java/com/aluracursos/desafio2/model/DatosCantante.java
@@ -1,17 +1,37 @@
 package com.aluracursos.desafio2.model;
 
+import jakarta.persistence.*;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
+@Table(name = "cantantes")
 public class DatosCantante {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    @Column(unique = true)
     private String nombre;
+    @Enumerated(EnumType.STRING)
     private TipoCantante tipo;
+    @OneToMany(mappedBy = "cantante", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<DatosCancion> canciones = new ArrayList<>();
+
+    public DatosCantante(){}
 
     public DatosCantante(String nombre, TipoCantante tipo){
         this.nombre = nombre;
         this.tipo = tipo;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
     }
 
     public String getNombre() {
